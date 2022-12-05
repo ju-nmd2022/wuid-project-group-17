aae379dc3cc24e469d79d653e038ec692ba1cf2c
Jie C <117114870+jie5566@users.noreply.github.com>
2022-12-05 23:26:59 +0100

    adding links between each pages

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -1,6 +1,7 @@
 // the star rating is learned from https://www.youtube.com/watch?v=fQIerHqB71w&t=11s
 const allStars = document.querySelectorAll(".star");
 
+
 allStars.forEach((star, i) => {
     star.onclick = function() {
        let current_star_level = i +1;
@@ -11,7 +12,43 @@ allStars.forEach((star, i) => {
         }else {
         star.innerHTML= "&#9734";
        } } ) 
+       localStorage.rate("rating","true")
     }
 }
-
 )
+
+
+let search = document.getElementById("search")
+let showInput = document.getElementById("search-input")
+
+search.addEventListener("click", function(){
+    showInput.style.visibility= "visible";
+})
+
+// the following searching methord is learned from https://www.youtube.com/watch?v=qp7PdguA0Vg
+// https://www.youtube.com/watch?v=TlP5WIxVirU
+
+// document.addEventListener("DOMContentLoaded", ()=> {
+
+//     document.querySelectorAll(".dish-name").forEach(inputField => {
+
+//     }) 
+
+// });
+
+const searchInput = document.querySelectorAll("[data-search]")
+let recipe = document.getElementById("dish")
+
+let recipes = []
+
+searchInput.addEventListener("input", e => {
+    const value = e.target.value.toLowerCase()
+    recipes.forEach(recipe => {
+        const isVisible =
+        dish.toLowerCase().includes(value)
+        recipes.toggle("hide", !isVisible)
+    }
+
+    )
+
+})
