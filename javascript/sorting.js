895f613b24664dcc00baaf6e9b47ed4797c70220
Jie C <117114870+jie5566@users.noreply.github.com>
2022-12-06 22:03:22 +0100

    merge with home page

--- javascript/sorting.js
+++ javascript/sorting.js
@@ -1,5 +1,6 @@
 // the star rating is learned from https://www.youtube.com/watch?v=fQIerHqB71w&t=11s
 //const allStars = dish.querySelectorAll(".star");
+// I also get help from Lukas
 
 let dish = document.querySelectorAll(".dish")
 for (let i = 0; i < dish.length; i++) {
@@ -21,39 +22,40 @@ for (let i = 0; i < dish.length; i++) {
     )
 }
 
-localStorage.getItem
+
+const hamburger = document.querySelector(".hamburger");
+const hamburger_icon = hamburger.querySelector("span");
+const mobile_menu = document.querySelector(".mobile-menu");
+
+hamburger.addEventListener("click", () => {
+  hamburger_icon.innerText =
+    hamburger_icon.innerText === "menu" ? "close" : "menu";
+
+  mobile_menu.classList.toggle("is-open");
+});
 
 
-let search = document.getElementById("search")
-let showInput = document.getElementById("search-input")
+// the following code is for the search bar, so far, very hard for me to understand, I gave up
 
-search.addEventListener("click", function(){
-    showInput.style.visibility= "visible";
-})
+// let search = document.getElementById("search")
+// let showInput = document.getElementById("search-input")
 
-// the following searching methord is learned from https://www.youtube.com/watch?v=qp7PdguA0Vg
-// https://www.youtube.com/watch?v=TlP5WIxVirU
+// search.addEventListener("click", function(){
+//     showInput.style.visibility= "visible";
+// })
 
-// document.addEventListener("DOMContentLoaded", ()=> {
+// const searchInput = document.querySelectorAll("[data-search]")
+// let recipe = document.getElementById("dish")
 
-//     document.querySelectorAll(".dish-name").forEach(inputField => {
+// let recipes = []
 
-//     }) 
+// searchInput.addEventListener("input", e => {
+//     const value = e.target.value.toLowerCase()
+//     recipes.forEach(recipe => {
+//         const isVisible =
+//         dish.toLowerCase().includes(value)
+//         recipes.toggle("hide", !isVisible)
+//     }
+//     )
 
-// });
-
-const searchInput = document.querySelectorAll("[data-search]")
-let recipe = document.getElementById("dish")
-
-let recipes = []
-
-searchInput.addEventListener("input", e => {
-    const value = e.target.value.toLowerCase()
-    recipes.forEach(recipe => {
-        const isVisible =
-        dish.toLowerCase().includes(value)
-        recipes.toggle("hide", !isVisible)
-    }
-    )
-
-})
+// })
